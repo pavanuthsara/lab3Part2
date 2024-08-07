e95b58d7531282c68d1575da41b8aee2133d219f
Pavan Uthsara <uthsarapavan@gmail.com>
2024-08-07 11:47:17 +0530

    Lab 03 - example 08

---
+++ src/myPackage/Cat.java
@@ -0,0 +1,10 @@
+package myPackage;
+
+public class Cat extends Pet{
+	private int livesLeft;
+	
+	public Cat(String n, String o, int a, int l) {
+		super(n,o,a);
+		this.livesLeft = l;
+	}
+}//end of the Cat class
---
+++ src/myPackage/Main.java
@@ -0,0 +1,13 @@
+package myPackage;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Pet p = new Pet("Lissie", "Smith", 3);
+		p.showDetails();
+		
+		Cat c = new Cat("Kyan", "Silva", 4, 4);
+		c.showDetails();
+	}
+}// end of demo class
